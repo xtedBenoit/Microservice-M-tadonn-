9acfe6f532a1e8dd803b045d0630dd1dd5cf4573
teddy AMAÏZO <benoisTeddy110@gmail.com>
2024-09-10 10:58:02 +0000

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var path = require('path')
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -12,6 +16,30 @@ app.get('/', function (req, res) {
 });
 
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'Aucun fichier uploadé' });
+  }
+
+  const fileDetails = {
+    name: req.file.filename,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+
+  res.json(fileDetails);
+});
 
 
 const port = process.env.PORT || 3000;
